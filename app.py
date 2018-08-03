9e5166dd4d607bb67452d7b935402b9408c4567e
Anne Marie Johnston <annemarie.johnston@outlook.com>
2018-08-03 06:51:31 +0000

    added routing add_task

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def get_tasks():
     tasks=mongo.db.tasks.find())
 
 
-# @app.route('/')
-# def hello():
-#     return "Hello World ...again"
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html")
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
